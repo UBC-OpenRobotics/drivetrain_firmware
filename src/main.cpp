e6d5435d3e760d255e2b4ffb8b052ae7706a910e
My Name <setimalek@gmail.com>
2023-02-04 15:32:42 -0800

    arduino set up

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,44 @@
 #include <Arduino.h>
+#include <functions.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+// PIN DEFINITION
+#define LED 2
+#define DIR 18  
+#define PUL 19 //pulse
+#define SW  15
+#define ENA 21
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//CONSTANT
+#define PRESCALAR 20
+#define COUNT 50
+#define MOTOR_ENABLE 0
+#define MOTOR_DISABLE 1
+
+// GLOBAL ASSIGNMENT
+bool  volatile state;
+int   volatile counter, timer_counter;
+
+
+void setup() 
+{
+  Serial.begin(115200);
+  
+  //PIN SETUP
+  pinMode(LED, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  pinMode(PUL, OUTPUT);
+  pinMode(ENA, OUTPUT);
+
+  pinMode(SW,  INPUT_PULLUP);
+  digitalWrite(ENA,MOTOR_DISABLE);
+  state =0;
+  counter =50;
+  timer_counter =0;
+  
+  // Timer ISR
+  hw_timer_t* timer = timerBegin(0, PRESCALAR, true);
+  timerAttachInterrupt(timer, &timer_isr, true);
+  timerAlarmWrite(timer, COUNT, true);
+  timerAlarmEnable(timer);
 }
+ 
